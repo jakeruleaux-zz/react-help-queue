4dd5f5782e0333e986514ce561cc162033c6e832
jacob ruleaux and Michelle Poterek <jacob@>
2017-09-11 16:02:18 -0700

    bind click event for request help button

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -5,10 +5,12 @@ class NewTicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {formVisibleOnPage: false};
+    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this);
   }
 
   handleDisplayingNewTicketForm(event){
-    console.log("new ticket button clicked!")
+    console.log("new ticket button clicked!");
+    this.setState({formVisibleOnPage: true});
   }
 
   render(){
