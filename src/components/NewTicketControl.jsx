6ae5162ed1e6baac49664384ff8932b87f491a54
jacob ruleaux and Michelle Poterek <jacob@>
2017-09-11 16:26:39 -0700

    new ticket hides and shows appropriately

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,23 +1,33 @@
 import React from "react";
+import NewTicketForm from "./NewTicketForm";
 
 class NewTicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {formVisibleOnPage: false};
-    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this);
+    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this)
   }
 
   handleDisplayingNewTicketForm(event){
-    console.log("new ticket button clicked!");
     this.setState({formVisibleOnPage: true});
   }
 
   render(){
+    let formAreaContent = null;
+    if (this.state.formVisibleOnPage){
+      formAreaContent = <NewTicketForm/>
+    } else {
+      formAreaContent = <button onClick={this.handleDisplayingNewTicketForm}>Request Help</button>;
+    }
+
     return (
-      <button onClick={this.handleDisplayingNewTicketForm}>Request Help</button>
+      <div>
+        {formAreaContent}
+      </div>
     );
   }
+
 
 }
 
